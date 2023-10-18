78a667fceaa21a7b04e67e9840806f9ba1515342
dnyaneshskamthe <dnyanesh451989@gmail.com>
2023-10-18 14:25:23 +0530

    added search and dropdown button

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,19 +1,36 @@
-import ProductTable from "./ProductTable"
-
+import ProductTable from "./ProductTable";
 
 const Products = () => {
   return (
-    <div className=" card  mx-5 mt-1 border-0" style={{}}>
-    <div className="product">
-      <div className="d-flex flex-column">
-      <span className="fs-4 fw-bold ms-3 pt-2">Product Sell</span>
+    <div className="card mx-5 mt-1 border-0" style={{}}>
+      <div className="product">
+        <div className="product-header d-flex">
+          <div className="d-flex flex-column col-lg-8 col-md-6 col-sm-3">
+            <span className="fs-4 fw-bold ms-3 pt-2">Product Sell</span>
+          </div>
+          <div className="product-search">
+            <input
+              type="text"
+              placeholder="search"
+              className="mt-2 ms-2 ps-3 rounded"
+              style={{ width: "10rem", height: "2.4rem" }}
+            />
+          </div>
+          <div className="dropdown mt-2 ms-2">
+            <select name="cars" id="cars" className="rounded p-2 border-secondry">
+              <option value="30days">last 30 days</option>
+              <option value="60days">last 60 days</option>
+              <option value="90days">last 90 days</option>
+              <option value="120days">last 120 days</option>
+            </select>
+          </div>
+        </div>
       </div>
-      <div>
-        <ProductTable/>
+      <div className="table-wrapper">
+        <ProductTable />
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
